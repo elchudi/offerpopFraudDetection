594bb57b8f032d27382221139b14bd5e47922f6a
elchudi <gustavogiudici@gmail.com>
2012-11-26 00:02:46 +0100

    print out name and votes

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 import data
+import urllib2
 
-print data.urls
+
+for k,v in data.urls.items():
+    response = urllib2.urlopen(v)
+    html = response.read()
+    pos = html.find('"VoteCount"')
+    html_piece = html[pos: pos +20]
+    print k, html_piece[html_piece.find(">") + 1: html_piece.find("<")]
