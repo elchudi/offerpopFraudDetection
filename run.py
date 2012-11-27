d57a91a3a8065db80d69d727272f22b8ce0f590c
ggiudici <gustavog@elchudi.xen.prgmr.com>
2012-11-27 18:36:12 +0000

    try catch implemented

--- run.py
+++ run.py
@@ -5,13 +5,15 @@ import time
 with open("test.txt", "a") as myfile:
     while(True):
         for k,v in data.urls.items():
-            response = urllib2.urlopen(v)
-            html = response.read()
-            pos = html.find('"VoteCount"')
-            html_piece = html[pos: pos +20]
-            #print k,",", html_piece[html_piece.find(">") + 1: html_piece.find("<")],",", time.time()
-            toapp = ",".join([ k, html_piece[html_piece.find(">") + 1: html_piece.find("<")],str( time.time())])
-            myfile.write(toapp)
-            myfile.write("\n")
-
+            try:
+                response = urllib2.urlopen(v)
+                html = response.read()
+                pos = html.find('"VoteCount"')
+                html_piece = html[pos: pos +20]
+                #print k,",", html_piece[html_piece.find(">") + 1: html_piece.find("<")],",", time.time()
+                toapp = ",".join([ k, html_piece[html_piece.find(">") + 1: html_piece.find("<")],str( time.time())])
+                myfile.write(toapp)
+                myfile.write("\n")
+            except:
+                time.sleep(2)
 
